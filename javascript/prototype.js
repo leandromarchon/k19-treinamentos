35bec7de92d222083107dfbd80f4b35a011de06b
Leandro Marchon <marchonleandro@gmail.com>
2018-08-18 19:40:22 -0300

    Adicionado arquivos prototype.html e prototype.js

---
+++ javascript/prototype.js
@@ -0,0 +1,9 @@
+var curso = {
+    sigla : "K11",
+    nome : "Orientação a Objetos em Java"
+};
+
+var novo_curso = Object.create(curso);
+
+console.log(novo_curso.sigla);
+console.log(novo_curso.nome);
