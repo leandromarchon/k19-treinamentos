83fb758a237ac80e46805cf5838315d9c4fbbd9f
Leandro Marchon <marchonleandro@gmail.com>
2018-08-19 20:26:30 -0300

    Adicionado arquivos array-lenght.html e array_lenght.js

---
+++ javascript/array_lenght.js
@@ -0,0 +1,8 @@
+var vazio = [];
+var cursos = ["k11", "k12", "k21", "k22", "k23", "k31", "k32"];
+
+console.log(vazio[0]);
+console.log(cursos[0]);
+
+console.log(vazio.length);
+console.log(cursos.length);
