bca7cbd76f91b7b34310815643563ca07e518a3f
Leandro Marchon <marchonleandro@gmail.com>
2018-08-18 21:32:20 -0300

    Adicionado arquivos soma.html e soma.js

---
+++ javascript/soma.js
@@ -0,0 +1,12 @@
+var soma = function(){
+    var soma = 0;
+
+    for(var i = 0; i < arguments.length; i++){
+        soma += arguments[i];
+    }
+
+    return soma;
+};
+
+var resultado = soma(2, 4, 6, 8);
+console.log(resultado);
