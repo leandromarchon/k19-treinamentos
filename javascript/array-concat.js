17aafd14fa87e83a4c6818c43bf613800b774b3a
Leandro Marchon <marchonleandro@gmail.com>
2018-08-19 21:40:19 -0300

    Adicionado arquivos array-concat.html e array-concat.js

---
+++ javascript/array-concat.js
@@ -0,0 +1,8 @@
+var formacaoJava = ["k11", "k12"];
+var formacaoJavaAvancado = ["k21", "k22", "k23"];
+
+var formacaoCompleta = formacaoJava.concat(formacaoJavaAvancado);
+
+for(var i = 0; i < formacaoCompleta.length; i++){
+    console.log(formacaoCompleta[i]);
+}
