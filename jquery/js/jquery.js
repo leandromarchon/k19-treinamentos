c95c43a0142c06d4152e2d652a623eacc04134ce
Leandro Marchon <marchonleandro@gmail.com>
2018-09-22 19:46:38 -0300

    Removido arquivos

--- jquery/js/jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#esconder").click(function(){
-        $("div").slideUp();
-    });
-
-    $("#mostrar").click(function(){
-        $("div").slideDown();
-    });
-});
